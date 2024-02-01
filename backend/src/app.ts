2118f845548179135fca9f219ae652c4925421ad
Wesley Moraes"git config --global user.name Wesley <wesleyjava@gmail.com>
2024-02-01 12:44:39 -0300

    initial server in backend

---
+++ backend/src/app.ts
@@ -0,0 +1,41 @@
+import http from "http";
+import { Server, Socket } from "socket.io";
+import express, { Application } from "express";
+
+class App {
+  private app: Application;
+  private http: http.Server;
+  private io: Server;
+
+  constructor() {
+    this.app = express();
+    this.http = new http.Server(this.app);
+    this.io = new Server(this.http);
+  }
+
+  public listen() {
+    this.app.listen(3333, () => {
+      console.log("Server running om port 3333");
+    });
+  }
+
+  public listenSocket() {
+    // escutar uma única rota específica
+    this.io.of("/streams").on("connection", this.socketEvents);
+  }
+
+  private socketEvents(socket: Socket) {
+    console.log("Socket connect: " + socket.id);
+    socket.on("subscribe", (data) => {
+      socket.join(data.roomId);
+
+      socket.broadcast.to(data.roomId).emit("chat", {
+        message: data.message,
+        userName: data.userName,
+        time: data.time,
+      });
+    });
+  }
+}
+
+export { App };
