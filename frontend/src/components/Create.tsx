160aa5fda0b36ba66eb47cb13463e33a9f5f76ca
Wesley Moraes"git config --global user.name Wesley <wesleyjava@gmail.com>
2024-02-03 18:49:21 -0300

    feat: adding socket of the communication banck and front

--- frontend/src/components/Create.tsx
+++ frontend/src/components/Create.tsx
@@ -1,14 +1,31 @@
+"use client";
+import { useRouter } from "next/navigation";
 import Button from "./Button";
-import { useRef } from "react";
 import { Input } from "./Input";
+import { FormEvent, useRef } from "react";
 
 export default function CreateRoom() {
   const name = useRef<HTMLInputElement>(null);
+  const router = useRouter();
+
+  const handleCreateRoom = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (name.current && name.current.value !== "") {
+      sessionStorage.setItem("username", name.current.value);
+      const roomId = generateRandomString();
+      console.log("handleCreateRoom - room: ", roomId);
+      router.push(`/room/${roomId}`);
+    }
+  };
+
+  function generateRandomString() {
+    return Math.random().toString(36).substring(2, 7);
+  }
 
   return (
-    <>
+    <form className="space-y-8" onSubmit={(e) => handleCreateRoom(e)}>
       <Input placeholder="Seu nome" type="text" ref={name} />
-      <Button type="button">Entrar</Button>
-    </>
+      <Button type="submit">Entrar</Button>
+    </form>
   );
 }
